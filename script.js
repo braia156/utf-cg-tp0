7ff4fffd369673a6d342dba3a204d600ff2e097d
bryanalves04 <bryan.alves.estoque@gmail.com>
2026-08-19 10:59:19 -0300

    texto do botao 2

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ botao.addEventListener('click', () => {
     if (nitroAtivado) {
         foto.style.transition = 'transform 0.5s cubic-bezier(0.68, -0.55, 0.27, 1.55)';
         foto.style.transform = 'rotate(360deg) scale(1.15)';
-        botao.innerText = '💨 Pisar no freio!';
+        botao.innerText = '💨 Botãozinho do N₂O!';
         botao.style.backgroundColor = '#2b8a3e';
     } else {
         foto.style.transform = 'rotate(0deg) scale(1)';
-        botao.innerText = 'Injetar Nitro 💨';
+        botao.innerText = 'Droga! é o braia 💨';
         botao.style.backgroundColor = '';
     }
 });
